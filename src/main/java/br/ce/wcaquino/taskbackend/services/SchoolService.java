918d947c97f7734945b4411756e0ceecd4e83ceb
Guilherme <guilherme.fernandes@inmetrics.com.br>
2022-01-31 16:54:05 -0300

    ValidationException when grade invalid

--- src/main/java/br/ce/wcaquino/taskbackend/services/SchoolService.java
+++ src/main/java/br/ce/wcaquino/taskbackend/services/SchoolService.java
@@ -2,12 +2,16 @@ package br.ce.wcaquino.taskbackend.services;
 
 import br.ce.wcaquino.taskbackend.model.School;
 import br.ce.wcaquino.taskbackend.model.SchoolStatus;
+import br.ce.wcaquino.taskbackend.utils.ValidationException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SchoolService {
 
-    public School closedStatus(int grade){
+    public School closedStatus(int grade) {
+        if(grade > 100){
+            throw new ValidationException("Valr inválidooooooo!!!!");
+        }
         School school = new School(grade, SchoolStatus.whatStatus(grade));
         return school;
     }
